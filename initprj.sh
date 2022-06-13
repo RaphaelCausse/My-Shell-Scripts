f17148860eeb1dc47049a326c7327c4b8d1fac67
Raphael Causse <raphael.causse2@gmail.com>
2022-06-13 16:12:07 +0200

    refactor: options management and usage

--- initprj.sh
+++ initprj.sh
@@ -4,49 +4,73 @@ function usage {
     echo -e "\e[1mUsage:\e[0m\n\t\tinitprj [-h] [ -l language ] [ -n project_name ]\n"
     echo -e "\e[1mOptions:\e[0m"
     echo -e "\t\e[1m-h\e[0m\tPrint this help message and exit.\n"
-    echo -e "\t\e[1m-l\e[0m\tInit specified [language] project directory.\n\t\tSupported languages : C, C++/Cpp, WEB (HTML-CSS-JS).\n"
+    echo -e "\t\e[1m-l\e[0m\tInit specified [language] project directory.\n\t\tSupported languages : C, C++, Python, Web (HTML-CSS-JS).\n"
     echo -e "\t\e[1m-n\e[0m\tInit project directory as [project_name] directory.\n"
     echo -e "\e[1mDescription:\e[0m"
     echo -e "\tInit Project (initprj) is a tool to create and initalize a new project directory corresponding to a programming language and a project name."
 }
 
+# Color variables
 RED='\033[0;31m'
 NC='\033[0m'
-LANG=0
+lang=""
 
+# Get options
 while getopts "hl:n:" options; do
     case ${options} in
-        h)
+        h)  # Help
             usage && exit 0 ;;
-        l)
-            # C
+        l)  # Language
             if echo ${OPTARG} | grep -qi "^C$" ; then
-                cp -r /home/raphael/Modèles/initprj/C_prj/ C_Project && LANG=1
-            # C++
-            if echo ${OPTARG} | grep -qi "^CPP$" | grep -qi "^C++$" ; then
-                cp -r /home/raphael/Modèles/initprj/Cpp_prj/ Cpp_Project && LANG=1
-            # WEB DEV
+                lang="C"
+            elif echo ${OPTARG} | grep -qi "^C++$\|^CPP$" ; then
+                lang="C++"
+            elif echo ${OPTARG} | grep -qi "^PYTHON$\|^PY$" ; then
+                lang="Python"
             elif echo ${OPTARG} | grep -qi "^WEB$" ; then
-                cp -r /home/raphael/Modèles/initprj/WEB_prj/ WEB_Project && LANG=2
+                lang="Web Dev"
             else
                 echo -e "${RED}\e[1mError:\e[0m${NC} language not supported."
                 echo "Run « initprj -h » for more information."
                 exit 2
             fi ;;
-        n)
-            # C
-            if [ ${LANG} -eq 1 ]; then
-                mv C_Project/ ${OPTARG}/
-                PRJ_NAME=${OPTARG}
-                echo "# ${PRJ_NAME^^}" > ${PRJ_NAME}/README.md
-            # WEB DEV
-            elif [ ${LANG} -eq 2 ]; then
-                mv WEB_Project/ ${OPTARG}/
-            else
-                mkdir -p ${OPTARG}
-            fi 
-            echo "Project directory [${OPTARG}] has been created." ;;
+        n)  # Directory name
+            dir_name=${OPTARG} ;;
         *)
             usage && exit 1 ;;
     esac
 done
+
+# Using options to build project directory
+if [ ! -z "${lang}" ]; then
+    case ${lang} in
+        "C")
+            cp -r /home/raphael/Modèles/initprj/C_prj/ C_Project ;;
+        "C++")
+            cp -r /home/raphael/Modèles/initprj/Cpp_prj/ Cpp_Project ;;
+        "Python")
+            cp -r /home/raphael/Modèles/initprj/Py_prj/ Py_Project ;;
+        "Web Dev")
+            cp -r /home/raphael/Modèles/initprj/Web_prj/ Web_Project ;;
+        *)  ;;
+    esac
+fi       
+
+if [ ! -z "$dir_name" ]; then
+    case ${lang} in
+        "C")
+            mv C_Project/ ${dir_name} ;;
+        "C++")
+            mv Cpp_Project/ ${dir_name} ;;
+        "Python")
+            mv Py_Project/ ${dir_name} ;;
+        "Web Dev")
+            mv Web_Project/ ${dir_name} ;;
+        *)
+            mkdir -p ${dir_name} ;;
+    esac
+    echo "# ${dir_name^^}" >> ${dir_name}/README.md
+    echo "[${lang}] Project directory '${dir_name}' has been created."
+else
+    echo "[${lang}] Project directory has been created."
+fi
