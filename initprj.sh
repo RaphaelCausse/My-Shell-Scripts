0e796d2bec9cf591dfd5491da6eac61fbc3b938c
Raphael Causse <raphael.causse2@gmail.com>
2022-06-18 01:09:55 +0200

    fix: missing directory; add: install option

--- initprj.sh
+++ initprj.sh
@@ -5,14 +5,18 @@ function usage {
     echo -e "${GRN}${BOLD}NAME${NT}${NC}"
     echo -e "\tinitprj - initialize a specific language project directory\n"
     echo -e "${GRN}${BOLD}SYNOPSIS${NT}${NC}"
-    echo -e "\t./initprj.sh [${GRN}${BOLD}-h${NT}${NC}]"
+    echo -e "\t./initprj.sh [${GRN}${BOLD}-h${NT}${NC}|${GRN}${BOLD}-i${NT}${NC}]"
     echo -e "\t./initprj.sh [${GRN}${BOLD}-n${NT}${NC} <${CYAN}project_name${NC}>] ${GRN}${BOLD}-l${NT}${NC} <${CYAN}language${NC}>\n"
     echo -e "${GRN}${BOLD}OPTIONS${NT}${NC}"
     echo -e "\t${GRN}${BOLD}-h${NT}${NC}\tdisplay this help message and exit\n"
+    echo -e "\t${GRN}${BOLD}-i${NT}${NC}\tinstall this script in ~/.local/bin/ and move the initprj directory to ~/\n"
     echo -e "\t${GRN}${BOLD}-l${NT}${NC} ${CYAN}language${NC}"
     echo -e "\t\tinitialize a new ${CYAN}language${NC} project directory\n"
     echo -e "\t${GRN}${BOLD}-n${NT}${NC} ${CYAN}project_name${NC}"
     echo -e "\t\tname project directory as ${CYAN}project_name${NC}\n"
+    echo -e "${GRN}${BOLD}GUIDE${NT}${NC}"
+    echo -e "\t./initprj.sh -i"
+    echo -e "\t./initprj.sh -n Test_initprj -l python\n"
     echo -e "${GRN}${BOLD}DESCRIPTION${NT}${NC}"
     echo -e "\tInitprj is a tool to create and initialize a specific language project directory.\n\tSupported languages : C, C++, Python, Web (HTML, CSS, JS)\n"
     echo -e "${GRN}${BOLD}AUTHOR${NT}${NC}"
@@ -31,10 +35,15 @@ NC='\033[0m'
 lang=""
 
 # Get options
-while getopts "hl:n:" options; do
+while getopts "hil:n:" options; do
     case ${options} in
         h)  # Help
             usage && exit 0 ;;
+        i)  # Install
+            cp -u $0 ~/.local/bin/initprj
+            cp -ru initprj/ ~/.initprj/
+            echo "Added 'initprj' to ~/.local/bin and ready to be used as command 'initprj'"
+            exit 0 ;;
         l)  # Language
             if echo ${OPTARG} | grep -qi "^C$" ; then
                 lang="C"
@@ -46,7 +55,7 @@ while getopts "hl:n:" options; do
                 lang="Web Dev"
             else
                 echo -e "${RED}\e[1mError:\e[0m${NC} language ${OPTARG} is not supported."
-                echo "Run « initprj -h » for more information."
+                echo "Run « ./initprj.sh -h » for more information."
                 exit 2
             fi ;;
         n)  # Directory name
@@ -54,7 +63,7 @@ while getopts "hl:n:" options; do
                 dir_name=${OPTARG}
             else
                 echo -e "${RED}\e[1mError:\e[0m${NC} file or folder ${OPTARG} already exists here."
-                echo "Run « initprj -h » for more information."
+                echo "Run « ./initprj.sh -h » for more information."
                 exit 3
             fi ;;
         *)
@@ -66,13 +75,13 @@ done
 if [ ! -z "${lang}" ]; then
     case ${lang} in
         "C")
-            cp -r /home/raphael/Modèles/initprj/C_prj/ C_Project ;;
+            cp -r ~/.initprj/C_prj/ C_Project ;;
         "C++")
-            cp -r /home/raphael/Modèles/initprj/Cpp_prj/ Cpp_Project ;;
+            cp -r ~/.initprj/Cpp_prj/ Cpp_Project ;;
         "Python")
-            cp -r /home/raphael/Modèles/initprj/Py_prj/ Py_Project ;;
+            cp -r ~/.initprj/Py_prj/ Py_Project ;;
         "Web Dev")
-            cp -r /home/raphael/Modèles/initprj/Web_prj/ Web_Project ;;
+            cp -r ~/.initprj/Web_prj/ Web_Project ;;
         *)  ;;
     esac
 fi       
@@ -87,11 +96,9 @@ if [ ! -z "$dir_name" ]; then
             mv Py_Project/ ${dir_name} ;;
         "Web Dev")
             mv Web_Project/ ${dir_name} ;;
-        *)
+        *) 
             mkdir -p ${dir_name} ;;
     esac
     echo "# ${dir_name^^}" >> ${dir_name}/README.md
     echo "[${lang}] Project directory '${dir_name}' has been created."
-else
-    echo "[${lang}] Project directory has been created."
 fi
